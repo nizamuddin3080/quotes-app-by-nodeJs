98dd9d7f11524e4dfa57a0c9394fb0bd2e8eb51b
nizamuddin3080 <mdnizamuddin3080@gmail.com>
2021-01-23 01:04:38 +0600

    start app first commit

---
+++ app.js
@@ -0,0 +1,15 @@
+/*
+* 
+* Title : Basic Node.js exaple Application
+* Author : MD. Nizam Uddin Khondokar
+* Description : Simple Node js Application
+* Date : 23/3/2020
+*
+*/
+
+// Deperdency for application
+
+const MathLib = require('./lib/math');
+var quoteLib = require('./lib/quotes/quotes')
+
+
